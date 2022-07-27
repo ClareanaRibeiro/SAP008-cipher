df88d890276ea4956fa1e9e7d5ca8fed8dab6e16
ClareanaRibeiro <clacla.vaz.ribeiro@gmail.com>
2022-07-27 18:19:02 -0300

    TESTES CONCLUIDOS

--- src/cipher.js
+++ src/cipher.js
@@ -4,6 +4,10 @@ function encode(offset, string){
 
     let texto = "";
 
+    if (typeof offset !== "number" || typeof string !== "string"){
+      throw new TypeError
+    }
+
     for (let c=0; c<string.length; c++){   
        let processo= ((string.charCodeAt(c)-65+offset)%26)+65; 
        texto += String.fromCharCode(processo);
@@ -17,6 +21,10 @@ function encode(offset, string){
 function decode(offset,string){
 
   let texto = "";
+
+  if (typeof offset !== "number" || typeof string !== "string"){
+    throw new TypeError
+  }
 
   for (let c=0; c<string.length; c++){
     let processo= ((string.charCodeAt(c)-90-offset)%26)+90; 
